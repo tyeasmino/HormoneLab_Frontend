0f085d540e8ae579c262a050fec3c682b3fb7859
tyeasmino <tyeasmino@gmail.com>
2025-03-06 18:19:30 +0600

    HomePage Mobile View

--- src/components/ClimateDisease.jsx
+++ src/components/ClimateDisease.jsx
@@ -7,7 +7,7 @@ const ClimateDisease = () => {
         <section className='py-10'>
             <h1 className='max-w-screen-xl md:px-20 mx-auto p-5 flex text-2xl font-bold'>Climate Change and Health</h1>
             <section className='max-w-screen-xl md:px-20 gap-10 mx-auto p-5 flex flex-col md:flex-row items-center justify-between'>
-                <div className='md:w-1/2 h-[280px] flex flex-col gap-3 justify-between'>
+                <div className='md:w-1/2 h-[280px] order-2 md:order-1 flex flex-col gap-3 justify-between'>
 
                     <p className='text-gray-400 text-justify'>Climate change is exacerbating health risks in urban areas. Bangladesh, in particular, faces challenges such as increased air pollution, severe flooding, and outbreaks of waterborne diseases. These environmental changes contribute to respiratory issues and the spread of infectious diseases, necessitating integrated climate-health resilience strategies. 
                     </p>
@@ -18,7 +18,7 @@ const ClimateDisease = () => {
                         </p>
                     </Link>
                 </div>
-                <div className='md:w-1/2'>
+                <div className='md:w-1/2 order-1 md:order-2'>
                     <img className='h-[280px]' src={climate} alt="" />
                 </div>
             </section>
